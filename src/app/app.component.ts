bd56f07ce0368a4e6f261219c2e4bfe99cd04fcf
Brent Miller <brentwmiller93@gmail.com>
2017-09-28 22:18:09 -0400

    Setup base component for testing dungeon api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { DungeonService } from './services/dungeon.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
+	providers: [DungeonService]
 })
 export class AppComponent {
-  title = 'app';
+
+	class: any;
+
+	constructor(private dungeonService: DungeonService) {}
+	
+	ngOnInit() {
+		this.getClass(2);
+	}
+
+	public async getClass(id) {
+		this.dungeonService.getClass(id)
+			.subscribe((response) => {
+				this.class = response;
+				console.log(this.class);
+			});
+	}
 }
